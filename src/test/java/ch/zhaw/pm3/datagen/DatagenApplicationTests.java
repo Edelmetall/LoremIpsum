dac1dbc1324eb9f4d3dcd6f8d38461f01a245749
Edelmetall <chime.switzerland@gmail.com>
2022-03-05 12:24:13 +0100

    initial

---
+++ src/test/java/ch/zhaw/pm3/datagen/DatagenApplicationTests.java
@@ -0,0 +1,15 @@
+package ch.zhaw.pm3.datagen;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatagenApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Test Works");
+    }
+
+}
