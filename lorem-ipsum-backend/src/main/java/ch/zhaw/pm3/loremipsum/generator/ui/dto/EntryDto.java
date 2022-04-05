c845947bb86c55207c07096098dfefa6e9444dfe
Edelmetall <chime.switzerland@gmail.com>
2022-04-05 10:50:04 +0200

    #17 Draft

---
+++ lorem-ipsum-backend/src/main/java/ch/zhaw/pm3/loremipsum/generator/ui/dto/EntryDto.java
@@ -0,0 +1,13 @@
+package ch.zhaw.pm3.loremipsum.generator.ui.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EntryDto {
+
+    private String name;
+    private String data;
+
+}
--- lorem-ipsum-backend/src/test/java/ch/zhaw/pm3/loremipsum/LoremIpsumApplicationTests.java
+++ lorem-ipsum-backend/src/test/java/ch/zhaw/pm3/loremipsum/LoremIpsumApplicationTests.java
@@ -1,7 +1,16 @@
 package ch.zhaw.pm3.loremipsum;
 
+import ch.zhaw.pm3.loremipsum.generator.GenService;
+import ch.zhaw.pm3.loremipsum.generator.ui.dto.GenDto;
+import ch.zhaw.pm3.loremipsum.generator.ui.dto.RowTemplateDto;
+import ch.zhaw.pm3.loremipsum.generator.ui.dto.TemplateDto;
+import com.github.javafaker.HarryPotter;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.Set;
 
 @SpringBootTest
 class LoremIpsumApplicationTests {
@@ -11,4 +20,28 @@ class LoremIpsumApplicationTests {
         System.out.println("Test Works");
     }
 
+    @Autowired
+    private GenService genService;
+
+    @Test
+    void setGenService() {
+        GenDto genDto = new GenDto();
+
+        genDto.setOutput("XML");
+        TemplateDto templateDto = new TemplateDto();
+
+        templateDto.setName("MyName");
+        templateDto.setId(1L);
+
+        RowTemplateDto rowTemplateDto1 = new RowTemplateDto();
+        rowTemplateDto1.setName("FirstName");
+
+        RowTemplateDto rowTemplateDto2 = new RowTemplateDto();
+        rowTemplateDto2.setName("LastName");
+
+
+        templateDto.setRowTemplateDtoSet(Set.of(rowTemplateDto1, rowTemplateDto2));
+        genDto.setTemplateDto(templateDto);
+        genService.generateStuff(genDto);
+    }
 }
